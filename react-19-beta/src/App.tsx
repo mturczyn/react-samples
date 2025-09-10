52d87c830a4e32345091855033be09b7c8c0bb40
Michal Turczyn <turek1992@o2.pl>
2025-09-10 10:33:10 +0200

    Added example of Activity from React preview, however broke example with use hook. It worked with canary version which is updated to experimental with this commit

--- react-19-beta/src/App.tsx
+++ react-19-beta/src/App.tsx
@@ -1,23 +1,50 @@
-import { Suspense, use } from 'react'
+import { Suspense, use, useState, unstable_Activity as Activity } from 'react'
 import './App.css'
 
 function App() {
     return (
-        <Suspense fallback="Is loading...">
-            <AsyncComponent />
-        </Suspense>
+        <>
+            <Suspense fallback="Is loading...">
+                <AsyncComponent title="Main app async component" />
+            </Suspense>
+            {/* <ActivityExample /> */}
+        </>
     )
 }
 
 export default App
 
-function AsyncComponent() {
+function AsyncComponent({ title }: { title: string }) {
     const data = use<string>(
         new Promise((resolve) => {
+            console.log(title, 'Starting the promise in async component')
             setTimeout(() => {
                 resolve('foo')
-            }, 3000)
+            }, 1000)
         })
     )
-    return <div>Asyn data: {data}</div>
+    return (
+        <>
+            <h1>{title} (check logs related to this)</h1>
+            <div>Async data: {data}</div>
+        </>
+    )
+}
+
+function ActivityExample() {
+    const [visible, setVisible] = useState(false)
+
+    return (
+        <>
+            <button onClick={() => setVisible(!visible)}>
+                Toggle Visibility
+            </button>
+            {visible && (
+                <AsyncComponent title="Async Component without activity" />
+            )}
+            <Activity mode={visible ? 'visible' : 'hidden'}>
+                <AsyncComponent title="Async Component WITH activity" />
+            </Activity>
+        </>
+    )
 }
